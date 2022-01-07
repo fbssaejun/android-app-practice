79f5d8aa52145f100e2278ee7c325a9c919595a5
Anthony Kim <fbssaejun@gmail.com>
2022-01-07 09:46:53 -0500

    create toast that displays the input text when entered

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         EditText t = findViewById(R.id.source);
         String input = t.getText().toString();
         ((TextView)findViewById((R.id.output))).setText(input);
-
-        Log.d("info", input);
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show();
     }
 }
