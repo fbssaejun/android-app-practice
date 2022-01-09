c482c58824c67cd6114942465769b714ac6e5f45
Anthony Kim <fbssaejun@gmail.com>
2022-01-09 16:31:09 -0500

    implement butterknife

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.titleText) TextView appTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        appTitle.setText("This is the title");
         setTitle("Home");
     }
 
